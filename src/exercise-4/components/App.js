d396004e4c8cd0c980bcdbf182c55c8554fbe3e3
xiao luo <xiao.luo@thoughtworks.com>
2020-09-24 01:15:30 +0800

    [luoxiao] Complete homework

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from "react-router";
+import {Route, Switch} from "react-router";
 import Home from "./Home";
 import About from "./About";
 import User from "./User";
@@ -9,12 +9,16 @@ import NotMatch from "./NotMatch";
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Router>
+      
+      <div className="app"> 
+        <Router>      
+          <Switch>
             <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
+            <Route path='/' component={Home} />            
+            <Route path='/:^\d+$' component={User} />
+            <Route path='/about' component={User} />
             <Route path='/about' component={About} />
+          </Switch>
         </Router>
       </div>
     );
